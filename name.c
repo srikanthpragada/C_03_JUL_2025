4b26964382bfad50542e130107b956e863b82547
srikanthpragada <srikanthpragada@gmail.com>
2025-07-05 09:28:53 +0530

    05-JUL

---
+++ name.c
@@ -0,0 +1,10 @@
+// Program to print name and phone number
+// Date : 05-JUL-2025
+
+#include <stdio.h>
+
+void main()
+{
+    printf("Srikanth Technologies\n");
+    printf("9059057000");
+}
---
+++ square.c
@@ -0,0 +1,19 @@
+// Program to take a number and print square
+// Date : 05-JUL-2025
+
+#include <stdio.h>
+
+void main()
+{
+   int num, result;  // Variables
+
+      // Input
+      printf("Enter a number :");
+      scanf("%d", &num);
+
+      // Process
+      result = num * num;
+
+      // Output
+      printf("Square : %d", result);
+}
